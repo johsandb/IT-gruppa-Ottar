683831df57fb05386f57b7bd205cc1d0e0d1bb4f
prebentjemsland <prebt16@student.uia.no>
2017-01-30 13:47:04 +0100

    la til navn

--- hello.go
+++ hello.go
@@ -5,8 +5,17 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("Hei, jeg heter Martin.")
+<<<<<<< Updated upstream
 	fmt.Println("Hei, mitt navn er Kristian")
 	fmt.Println("Hei, mitt navn er Marius")
   fmt.Println("Hei, mitt navn er Christoffer")
 	fmt.Println("Hei, mitt navn er Frode")
+=======
+	fmt.Println ("Hei, mitt navn er Kristian")
+	fmt.Println ("Hei, mitt navn er Marius")
+	fmt.Println ("Hei, mitt navn er Frode")
+<<<<<<< Updated upstream
+=======
+	fmt.Println ("Hei, mitt navn er Preben")
+	fmt.Println ("Gutta kødder")
 }
