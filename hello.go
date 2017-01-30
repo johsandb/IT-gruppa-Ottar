4dcbacfab7caf7e2fea75b38d999b0e061405449
Christoffer Lislevand <chrirl16@student.uia.no>
2017-01-30 13:37:17 +0100

    Jeg legger til meg selv

--- hello.go
+++ hello.go
@@ -5,11 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("Hei, jeg heter Martin.")
-	fmt.Println ("Hei, mitt navn er Kristian")
-	fmt.Println ("Hei, mitt navn er Marius")
-<<<<<<< HEAD
-  fmt.Println ("Hei, mitt navn er Christoffer")
-=======
-	fmt.Println ("Hei, mitt navn er Frode")
->>>>>>> origin/master
+	fmt.Println("Hei, mitt navn er Kristian")
+	fmt.Println("Hei, mitt navn er Marius")
+  fmt.Println("Hei, mitt navn er Christoffer")
+	fmt.Println("Hei, mitt navn er Frode")
 }
