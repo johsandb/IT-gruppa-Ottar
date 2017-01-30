7d2787141d9ff45cc246090fc436233136c257c5
Christoffer Lislevand <chrirl16@student.uia.no>
2017-01-30 15:08:15 +0100

    Update 2.0

--- hello.go
+++ hello.go
@@ -4,22 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, 世界")
-	fmt.Println("Hei, jeg heter Martin.")
+	fmt.Println("Hei, mitt navn er Martin.")
 	fmt.Println("Hei, mitt navn er Kristian")
 	fmt.Println("Hei, mitt navn er Marius")
   fmt.Println("Hei, mitt navn er Christoffer")
 	fmt.Println("Hei, mitt navn er Frode")
-<<<<<<< HEAD
-=======
-	fmt.Println ("Hei, mitt navn er Kristian")
-	fmt.Println ("Hei, mitt navn er Marius")
-	fmt.Println ("Hei, mitt navn er Frode")
-<<<<<<< Updated upstream
-=======
-	fmt.Println ("Hei, mitt navn er Preben")
-	fmt.Println ("Gutta kødder")
-	fmt.Println ("Hei, mitt navn er Joakim")
-=======
 	fmt.Println("Hei, mitt navn er Preben")
->>>>>>> origin/master
+	fmt.Println("Hei, mitt navn er Joakim")
 }
