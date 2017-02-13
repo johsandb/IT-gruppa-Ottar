8be529086ca2f664c92f9f5ccb0ea55b76ddb45e
Christoffer Lislevand <chrirl16@student.uia.no>
2017-02-13 16:41:24 +0100

    Test 3.0

--- Innlevering 2/Oppgave 3/main_sum.go
+++ Innlevering 2/Oppgave 3/main_sum.go
@@ -13,8 +13,15 @@ func main(){
 		argInput2, err := strconv.ParseFloat(arg2, 64)
     if err != nil {
         fmt.Println(err)
+        fmt.Println()
+        fmt.Println("Eksempel på hvordan å kjøre denne filen:")
+        fmt.Println("go run FILNAVN VALUE_1 VALUE_2")
+        fmt.Println()
+        fmt.Println("Programmet tar heller ikke imot bokstaver")
     }
 
+    fmt.Println()
     fmt.Println("Summen av " + arg1 + " + " + arg2 + " er:")
     fmt.Println(argInput1 + argInput2)
+    fmt.Println()
 }
