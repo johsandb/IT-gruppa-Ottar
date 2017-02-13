cf2240325241d7aa269a38aa53cddc0d68afea4f
Christoffer Lislevand <chrirl16@student.uia.no>
2017-02-13 16:13:42 +0100

    Test 2.0

--- Innlevering 2/Oppgave 3/main_sum.go
+++ Innlevering 2/Oppgave 3/main_sum.go
@@ -7,12 +7,6 @@ import (
 )
 
 func main(){
-  //Her har vi koden fra oppgave linken, men
-  //vi får denne feilmeldingen
-  //strconv.ParseFloat: parsing "E:\\Christoffer\\Documents\\bin\\Test.exe": invalid syntax
-  //Args[1] setter antall parametere
-  //Så bruker vi ParseFloat for å kovertere inputen som er String
-  //til int ellr tall som da kan brukes til regning i lnije 24
     arg1 := os.Args[1]
 		arg2 := os.Args[2]
     argInput1, err := strconv.ParseFloat(arg1, 64)
@@ -21,6 +15,6 @@ func main(){
         fmt.Println(err)
     }
 
-    fmt.Println("Dette er svaret dit:")
+    fmt.Println("Summen av " + argInput1 + " + " + argInput2 + " er:")
     fmt.Println(argInput1 + argInput2)
 }
