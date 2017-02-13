569d554c72be8e26958bd85585c4431808de9c2d
Christoffer Lislevand <chrirl16@student.uia.no>
2017-02-13 15:49:14 +0100

    main tset 1

--- Innlevering 2/Oppgave 3/main_sum.go
+++ Innlevering 2/Oppgave 3/main_sum.go
@@ -1,9 +1,27 @@
 package sum
 
-func SumInt8(a, b int8) int8 {
-	return a + b
-}
+import (
+  "fmt"
+  "math"
+  "os"
+  "strconv"
+)
 
-func SumFloat64(a, b float64) float64 {
-	return a + b
+func run(){
+  //Her har vi koden fra oppgave linken, men
+  //vi får denne feilmeldingen
+  //strconv.ParseFloat: parsing "E:\\Christoffer\\Documents\\bin\\Test.exe": invalid syntax
+  //Args[1] setter antall parametere
+  //Så bruker vi ParseFloat for å kovertere inputen som er String
+  //til int ellr tall som da kan brukes til regning i lnije 24
+    arg1 := os.Args[1]
+		arg2 := os.Args[2]
+    argInput1, err := strconv.ParseFloat(arg1, 64)
+		argInput2, err := strconv.ParseFloat(arg1, 64)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println("Dette er svaret dit:")
+    fmt.Println(argInput2 + argInput2)
 }
