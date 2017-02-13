bc37fadc8b8f2b8ae058638457a380aec67aa7f8
Christoffer Lislevand <chrirl16@student.uia.no>
2017-02-13 15:52:57 +0100

    main test 2

--- Innlevering 2/Oppgave 3/main_sum.go
+++ Innlevering 2/Oppgave 3/main_sum.go
@@ -16,7 +16,7 @@ func run(){
     arg1 := os.Args[1]
 		arg2 := os.Args[2]
     argInput1, err := strconv.ParseFloat(arg1, 64)
-		argInput2, err := strconv.ParseFloat(arg1, 64)
+		argInput2, err := strconv.ParseFloat(arg2, 64)
     if err != nil {
         fmt.Println(err)
     }
